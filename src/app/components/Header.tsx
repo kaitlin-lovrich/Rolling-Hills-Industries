ec934dab392b95d37e8ceb19ba3fd8e5205c1d74
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-10-29 12:27:05 -0700

    Adjust page menu bar icon to shrink upon page scroll

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -63,6 +63,7 @@ export function Header() {
                     >
                         <AnimatedMenuIcon
                             isModalOpen={isModalOpen}
+                            isScrolledToTop={isScrolledToTop}
                             setIsModalOpen={setIsModalOpen}
                         />
                     </span>
